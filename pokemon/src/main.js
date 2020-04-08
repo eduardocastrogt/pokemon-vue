b4f49acd6b4ea7bbcd8280cbf90be81e8ed672e0
Eduardo Castro <eduardocastrogt@gmail.com>
2020-04-08 17:31:44 -0600

    SVG: Tipos de Pokémon

--- pokemon/src/main.js
+++ pokemon/src/main.js
@@ -4,8 +4,8 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+//Bootstrap
 Vue.use(BootstrapVue, IconsPlugin)
-
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
